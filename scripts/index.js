4f089483cc536dda892e88c0452ebd6551247f88
Hong Shen Zhou <hong.shenzhou@gmail.com>
2015-01-16 14:15:36 +0200

    Jquery test

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,20 @@
 /*
   Javascript File for index.html @ /index.html
- */
+*/
+
+/* Checks if Javascript works */
+console.log("Javascript is working");
+
+/* Check if Jquery works */
+window.onload = function() {
+    if (window.jQuery) {
+
+        // jQuery is loaded
+        console.log("jQuery has loaded successfully!");
+
+    } else {
+
+        // jQuery is not loaded
+        console.log("Warning - jquery has not loaded somehow!");
+    }
+}
