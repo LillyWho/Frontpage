2105f2bdb2aa8339cad0f9db627cee62952165a8
Hong Shen Zhou <hong.shenzhou@gmail.com>
2015-02-12 19:48:45 +0200

    Basic Javascript Backbone for map selector

--- scripts/mapshowcase.js
+++ scripts/mapshowcase.js
@@ -7,40 +7,77 @@ console.log("Javascript is working");
 
 /* Check if Jquery works */
 window.onload = function() {
-    if (window.jQuery) {
 
-        // jQuery is loaded
-        console.log("jQuery has loaded successfully!");
+    /* Performs basic jquery checks */
+    var checks = function() {
+        if (window.jQuery) {
 
-    } else {
+            // jQuery is loaded
+            console.log("jQuery has loaded successfully!");
 
-        // jQuery is not loaded
-        console.log("Warning - jquery has not loaded somehow!");
+        } else {
+
+            // jQuery is not loaded
+            console.log("Warning - jquery has not loaded somehow!");
+        }
     }
 
-    /* Slides open website on load */
-    console.log("Sliding out panel");
-    $("#body").slideToggle("slow");
+    /* Sliding function slides panel out (assuming display:none;) */
+    var slideout = function(panel){
+        console.log("Sliding out " + panel + "!");
+        $(panel).slideToggle("slow");
+    }
 
-    /* Increases text shadow on mouseover */
-    $("#mapinstall-header").mouseover(function(){
-        console.log("Mouseover detected!");
-        $("#mapinstall-header").css("box-shadow", "0.5em 0.5em 0.5em 0.5em #000000");
-    });
-    $("#mapinstall-header").mouseout(function(){
-        console.log("Mouseout detected!");
-        $("#mapinstall-header").css("box-shadow", "0 0 0 0 #000000");
-    });
+    /* Increases text shadow function */
+    var highlighter = function(shadowed) {
+        /* Adds box shadow values to div on mouseover */
+        $(shadowed).mouseover(function(){
+            console.log("Mouseover detected at " + shadowed + "!");
+            $(shadowed).css("box-shadow", "1px 3px 5px 0px #000000");
+        });
 
+        /* Removes box sahdow values (resets to default) when mouse leaves */
+        $(shadowed).mouseout(function(){
+            console.log("Mouseout detected at " + shadowed + "!");
+            $(shadowed).css("box-shadow", "1px 1px 1px 0px #000000");
+        });
+    };
 
+    /* mapshowcase.html specific maphighlight caller */
+    var map_highlighter = function() {
+
+        /* id_array contains all the id identifiers that will be highlighted */
+        var id_array = [
+            "#mapinstall-header",
+            "#control-point",
+            "#capture-the-flag",
+            "#king-of-the-hill",
+            "#payload"
+        ];
+        var arrayLength = id_array.length;
+
+        for (var i = 0; i < arrayLength; i++) {
+            highlighter(id_array[i]);
+        }
+    }
 
     /* Opens map install panel on click */
-    $("#mapinstall-header").click(function(){
+    var map_install_clicker = function(){
+        $("#mapinstall-header").click(function(){
 
-        console.log("Panel Toggled");
+            console.log("mapinstall panel toggled");
+            slideout("#mapinstall-panel")
 
-        /* Slides open panel */
-        $("#mapinstall-panel").slideToggle("fast");
+        });
+    }
 
-    });
+    /* Function caller calls all functions */
+    var function_caller = function() {
+        checks();
+        slideout("#body");
+        map_highlighter();
+        map_install_clicker();
+    };
+
+    function_caller();
 }
